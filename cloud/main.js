ca6589baed76f1659ff4f7fa35bde4516e52f189
antonioatrust <aaadesignstudios@gmail.com>
2016-04-04 01:49:56 -0400

    Remove User Function

--- cloud/main.js
+++ cloud/main.js
@@ -36,7 +36,7 @@ Parse.Cloud.define("addFriendToFriendsRelation", function(request, response) {
 
                     //saved the user, now edit the request status and save it
                     friendRequest.set("status", "APPROVED");
-                    friendRequest.set("channelId", fromUserM.id+"channel"+toUserM.id);
+                    friendRequest.set("channelId", fromUserM.id+"$channel$"+toUserM.id);
                     friendRequest.save(null, {
 
                         success: function() {
@@ -50,6 +50,90 @@ Parse.Cloud.define("addFriendToFriendsRelation", function(request, response) {
                         }
 
                     });
+
+                },
+
+                error: function(error) {
+
+                 response.error(error);
+
+                }
+
+            });
+
+        },
+
+        error: function(error) {
+
+            response.error(error);
+
+        }
+
+    });
+
+});
+
+
+Parse.Cloud.define("removeFriendToFriendsRelation", function(request, response) {
+
+    Parse.Cloud.useMasterKey();
+
+    var friendRequestId = request.params.friendRequest;
+    var state = request.params.state;
+    var query = new Parse.Query("FriendRequest");
+
+    //get the friend request object
+    query.get(friendRequestId, {
+
+        success: function(friendRequest) {
+
+            //get the user the request was from
+            var fromUser = friendRequest.get("requestFrom");
+            //get the user the request is to
+            var toUser = friendRequest.get("requestTo");
+
+            var relationFrom = fromUser.relation("friends");
+            //add the user the request was to (the accepting user) to the fromUsers friends
+            relationFrom.remove(toUser);
+
+            var relationTo = toUser.relation("friends");
+            //add the user the request was to (the accepting user) to the fromUsers friends
+            relationTo.remove(fromUser);
+
+            //save the fromUser
+            fromUser.save(null, {
+
+                success: function() {
+
+                    toUser.save(null, {
+
+                    success: function() {
+
+                        //saved the user, now edit the request status and save it
+                        friendRequest.set("status", state);
+                        friendRequest.save(null, {
+
+                            success: function() {
+
+                                response.success("saved relation and updated friendRequest");
+                            }, 
+
+                            error: function(error) {
+
+                                response.error(error);
+                            }
+
+                        });
+
+                    },
+
+                    error: function(error) {
+
+                     response.error(error);
+
+                    }
+
+                });
 
                 },
 
